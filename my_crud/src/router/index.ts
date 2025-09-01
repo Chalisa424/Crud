3f003a874629c747a4c2aaec1b20332ca08a322f
Chalisa424 <>
2025-09-01 10:44:22 +0700

    build .env

---
+++ my_crud/src/router/index.ts
@@ -0,0 +1,9 @@
+// import { createRouter, createWebHistory } from 'vue-router'
+// import BlogCreateView from '../views/BlogCreateView.vue'
+// import BlogDetailView from '../views/BlogDetailView.vue'
+// import BlogsListView from '../views/BlogsListView.vue'
+// import BlogUpdateView from '../views/BlogUpdateView.vue'
+
+// const router = createRouter ({
+//     history: createWebHistory
+// })

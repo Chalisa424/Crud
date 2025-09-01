be44d91f394cafee9b9ce952381bbcfab347d4c4
Chalisa424 <>
2025-09-01 13:56:41 +0700

    create router

--- my_crud/src/router/index.ts
+++ my_crud/src/router/index.ts
@@ -1,9 +1,38 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import BlogCreateView from '../views/BlogCreateView.vue'
-// import BlogDetailView from '../views/BlogDetailView.vue'
-// import BlogsListView from '../views/BlogsListView.vue'
-// import BlogUpdateView from '../views/BlogUpdateView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import BlogCreateView from "../views/BlogCreateView.vue";
+import BlogsListView from "../views/BlogsListView.vue";
+import BlogDetailView from "../views/BlogDetailView.vue";
+import BlogUpdateView from "../views/BlogUpdateView.vue";
 
-// const router = createRouter ({
-//     history: createWebHistory
-// })
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes:[
+        {
+            //หน้ารายการบทความ
+            path:'/blog',
+            name: 'BlogList',
+            component: BlogsListView
+        },
+        {
+            //สร้างหน้าจอเพิ่มบทความ
+            path:'/blog/create'	,
+            name:'BlogCreate',
+            component: BlogCreateView
+        },
+        {
+            //สร้างหน้าแสดงข้อมูลบทความ
+            path:'/blog/:id',
+            name:'BlogDetailView',
+            component: BlogDetailView
+        },
+        {
+           //สร้างหน้าแสดงแก้ไขบทความ
+           path:'/blog/:id/update',
+           name: 'BlogUpdateView',
+           component: BlogUpdateView
+        }
+        
+    ]
+})
+
+export default router
